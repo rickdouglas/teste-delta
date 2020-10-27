5a4d595f85aa0bda263c0fbc60e5f0b51306468d
rickdouglas <rickmiranda23@gmail.com>
2020-10-27 18:03:59 -0300

    Iniciando backend da aplicação

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/users', (request , response) => {
+    return response.json({message : 'Hello World !!!'});
+});
+
+app.listen(3333);
